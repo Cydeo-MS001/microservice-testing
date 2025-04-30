be9cfce3fad920ad71014dbbeb1354af5e45a07c
mehmetCt <mehmet@cybertekschool.com>
2025-04-30 15:32:03 -0400

    Sample Test Implementation is started

--- src/test/java/com/ticketingApp/runner/CukesRunner.java
+++ src/test/java/com/ticketingApp/runner/CukesRunner.java
@@ -15,8 +15,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features" ,
         glue = "com/ticketingApp/steps",
-        dryRun = false,
-        tags=""
+        dryRun = true,
+        tags="@sample"
 )
 public class CukesRunner {
 
--- src/test/java/com/ticketingApp/steps/APIStepDefs.java
+++ src/test/java/com/ticketingApp/steps/APIStepDefs.java
@@ -1,11 +1,16 @@
 package com.ticketingApp.steps;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.List;
 
 
 public class APIStepDefs {
@@ -15,6 +20,45 @@ public class APIStepDefs {
     Response response;
     ValidatableResponse thenPart;
     JsonPath jp;
+
+    @Given("I am logged into the {string} as a {string}")
+    public void i_am_logged_into_the_as_a(String service, String role) {
+
+    }
+    @Given("Accept header is {string}")
+    public void accept_header_is(String acceptHeader) {
+
+    }
+    @Given("I set basePath for {string}")
+    public void i_set_base_path_for(String basePath) {
+
+    }
+    @When("I send GET request to {string} endpoint")
+    public void i_send_get_request_to_endpoint(String endpoint) {
+
+    }
+    @Then("status code should be {int}")
+    public void status_code_should_be(Integer expectedStatusCode) {
+
+    }
+    @Then("the {string} field should be {string}")
+    public void the_field_should_be(String path, String expectedValue) {
+
+    }
+    @Then("the {string} field should contains data")
+    public void the_field_should_contains_data(String path) {
+
+    }
+    @Then("{string} should be later than {string}")
+    public void should_be_later_than(String endDate, String startDate) {
+
+    }
+    @Then("following fields should not be null")
+    public void following_fields_should_not_be_null(List<String> paths) {
+
+    }
+
+
 
 }
 
