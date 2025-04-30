080ad6e73478db669e2c7a457f742c9054695a29
mehmetCt <mehmet@cybertekschool.com>
2025-04-30 16:37:31 -0400

    Sample is DONE

--- src/test/java/com/ticketingApp/runner/CukesRunner.java
+++ src/test/java/com/ticketingApp/runner/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features" ,
         glue = "com/ticketingApp/steps",
-        dryRun = true,
+        dryRun = false,
         tags="@sample"
 )
 public class CukesRunner {
