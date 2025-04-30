34fdd49582af8271551b97cb381fecdf05022e1e
mehmetCt <mehmet@cybertekschool.com>
2025-04-30 16:07:45 -0400

    Sample Test Implementation is started

--- src/test/java/com/ticketingApp/steps/APIStepDefs.java
+++ src/test/java/com/ticketingApp/steps/APIStepDefs.java
@@ -10,6 +10,7 @@ import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
 
 import java.util.List;
 
@@ -27,43 +28,53 @@ public class APIStepDefs {
 
         String baseURI = APIUtil.setBaseURI(service);
         String token = APIUtil.createToken(role);
-        givenPart=APIUtil.initRequest(token,baseURI);
+        givenPart = APIUtil.initRequest(token, baseURI);
 
     }
+
     @Given("Accept header is {string}")
     public void accept_header_is(String acceptHeader) {
         givenPart.accept(acceptHeader);
 
     }
+
     @Given("I set basePath for {string}")
     public void i_set_base_path_for(String serviceName) {
         givenPart.basePath(APIUtil.setBasePath(serviceName));
     }
+
     @When("I send GET request to {string} endpoint")
     public void i_send_get_request_to_endpoint(String endpoint) {
-
+        response = givenPart.when().get(endpoint);
+        jp = response.jsonPath();
+        thenPart = response.then();
+        LOG.info("Response body {}",response.prettyPrint());
     }
+
     @Then("status code should be {int}")
-    public void status_code_should_be(Integer expectedStatusCode) {
-
+    public void status_code_should_be(int expectedStatusCode) {
+        Assert.assertEquals(expectedStatusCode,response.statusCode());
     }
+
     @Then("the {string} field should be {string}")
     public void the_field_should_be(String path, String expectedValue) {
-
+       Assert.assertEquals(expectedValue,jp.getString(path));
     }
+
     @Then("the {string} field should contains data")
     public void the_field_should_contains_data(String path) {
 
     }
+
     @Then("{string} should be later than {string}")
     public void should_be_later_than(String endDate, String startDate) {
 
     }
+
     @Then("following fields should not be null")
     public void following_fields_should_not_be_null(List<String> paths) {
 
     }
-
 
 
 }
