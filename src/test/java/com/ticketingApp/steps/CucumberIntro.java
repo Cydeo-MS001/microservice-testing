529f0183ebd894a9068d80d718c18cc495d54c8c
mehmetCt <mehmet@cybertekschool.com>
2025-05-02 17:17:44 -0400

    Database read issue is fixed

--- src/test/java/com/ticketingApp/steps/CucumberIntro.java
+++ src/test/java/com/ticketingApp/steps/CucumberIntro.java
@@ -11,6 +11,8 @@ import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Objects;
+
 public class CucumberIntro {
 
 
@@ -30,30 +32,30 @@ public class CucumberIntro {
         ValidatableResponse thenPart;
 
         //    Given I am logged into the "fakestoreapp" as a "MANAGER"
-              // givenPart.header("Authorization",APIUtil.createToken("MANAGER"));
+        // givenPart.header("Authorization",APIUtil.createToken("MANAGER"));
 
         //    And Accept header is "application/json"
-              givenPart.accept("application/json");
+        givenPart.accept("application/json");
 
         //    And Path Param "id" is "71"
-              givenPart.pathParam("id","71");
+        givenPart.pathParam("id", "71");
 
         //    Add baseUrı
-              givenPart.baseUri("https://api.escuelajs.co");
+        givenPart.baseUri("https://api.escuelajs.co");
 
         //    When I send GET request to "api/v1/products/{id}" endpoint
-              response = givenPart.when().get("api/v1/products/{id}");
-              jp = response.jsonPath();
-              thenPart = response.then();
+        response = givenPart.when().get("api/v1/products/{id}");
+        jp = response.jsonPath();
+        thenPart = response.then();
 
         //    Then status code should be 200
-              Assert.assertEquals(200,response.statusCode());
-              thenPart.statusCode(200);
+        Assert.assertEquals(200, response.statusCode());
+        thenPart.statusCode(200);
 
         //    And the "id" field should be "71"
         //       (String path,String value)
 
-              thenPart.body("id",Matchers.is(71));
+        thenPart.body("id", Matchers.is(71));
 
         //    And the "category.name" field should be "Clothes"
     }
@@ -66,5 +68,28 @@ public class CucumberIntro {
 
         int rowCount = DB_Util.getRowCount();
         System.out.println("rowCount = " + rowCount);
+    }
+
+    @Test
+    public void dynamicData() throws InterruptedException {
+
+        Response response = null;
+        ;
+
+        String email = "";
+
+        for (int i = 0; i < 6; i++) {
+            response = RestAssured.get("https://35dd6236-da70-4507-8d18-3f2030ed0722.mock.pstmn.io/api/v1/projects");
+            JsonPath jp = response.jsonPath();
+            email = jp.getString("data.findAll {it.step=='FinalResult'}.email");
+            if(email.contains("@")){
+                break;
+            }
+            System.out.println(i);
+            Thread.sleep(10000);
+        }
+
+        System.out.println(email);
+
     }
 }
