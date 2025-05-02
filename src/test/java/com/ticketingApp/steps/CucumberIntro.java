cb66a60214ca929279b2f7deacddd54b96ea9231
mehmetCt <mehmet@cybertekschool.com>
2025-05-02 13:09:11 -0400

    task service scenarious added

---
+++ src/test/java/com/ticketingApp/steps/CucumberIntro.java
@@ -0,0 +1,59 @@
+package com.ticketingApp.steps;
+
+import com.ticketingApp.utility.api.APIUtil;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CucumberIntro {
+
+
+    //    Given I am logged into the "fakestoreapp" as a "MANAGER"
+    //    And Accept header is "application/json"
+    //    And Path Param "id" is "71"
+    //    When I send GET request to "api/v1/products/{id}" endpoint
+    //    Then status code should be 200
+    //    And the "id" field should be "71"
+
+    @Test
+    public void test() {
+
+        RequestSpecification givenPart = RestAssured.given().log().uri();
+        Response response;
+        JsonPath jp;
+        ValidatableResponse thenPart;
+
+        //    Given I am logged into the "fakestoreapp" as a "MANAGER"
+              // givenPart.header("Authorization",APIUtil.createToken("MANAGER"));
+
+        //    And Accept header is "application/json"
+              givenPart.accept("application/json");
+
+        //    And Path Param "id" is "71"
+              givenPart.pathParam("id","71");
+
+        //    Add baseUrı
+              givenPart.baseUri("https://api.escuelajs.co");
+
+        //    When I send GET request to "api/v1/products/{id}" endpoint
+              response = givenPart.when().get("api/v1/products/{id}");
+              jp = response.jsonPath();
+              thenPart = response.then();
+
+        //    Then status code should be 200
+              Assert.assertEquals(200,response.statusCode());
+              thenPart.statusCode(200);
+
+        //    And the "id" field should be "71"
+        //       (String path,String value)
+
+              thenPart.body("id",Matchers.is(71));
+
+        //    And the "category.name" field should be "Clothes"
+    }
+}
--- src/test/resources/features/integration/task_service.feature
+++ src/test/resources/features/integration/task_service.feature
@@ -12,3 +12,26 @@ Feature: Task Service Access and Manipulation
     When I send POST request to "/create" endpoint
     Then status code should be 201
     And the "message" field should be "Task is successfully created."
+
+
+  Scenario: Retrieving a task by valid taskCode
+    Given I am logged into the "task.service" as a "employee"
+    And Accept header is "application/json"
+    And Path Param "taskCode" is "VALID TASK CODE"
+    When I send GET request to "read/{taskCode}" endpoint
+    And the "data.taskCode" field should not be null
+
+  Scenario: Fetch tasks for a specific project
+    Given I am logged into the "task.service" as a "manager"
+    And Accept header is "application/json"
+    And Path Param "projectCode" is "VALID PROJECT CODE"
+    When I send GET request to "/read/all/{projectCode}" endpoint
+    And status code should be 200
+    And the "message" field should be "Tasks are successfully retrieved."
+    And the "data" field should contains data
+    And following fields should not be null
+      | taskCode         |
+      | taskSubject      |
+      | taskStatus       |
+      | projectCode      |
+      | assignedEmployee |
